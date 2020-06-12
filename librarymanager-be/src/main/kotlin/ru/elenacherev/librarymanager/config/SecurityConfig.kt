fd92d243cfe8cd9d5cbbc2457f8ebaa6a624caaa
ElenaCherev <owle86@bk.ru>
2020-06-12 20:53:10 +0400

    zuru -> librarymanager

--- librarymanager-be/src/main/kotlin/ru/elenacherev/librarymanager/config/SecurityConfig.kt
+++ librarymanager-be/src/main/kotlin/ru/elenacherev/librarymanager/config/SecurityConfig.kt
@@ -1,10 +1,19 @@
 package ru.elenacherev.librarymanager.config
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
-@EnableGlobalAuthentication
 @EnableWebSecurity
-class SecurityConfig
+class SecurityConfig(
+) : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+            .antMatchers("/")
+            .permitAll()
+    }
+}
